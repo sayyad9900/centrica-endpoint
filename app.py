2718098511d508c1f58f8e1445e5658719c0f421
sayyad9900 <sayyad.azharuddin@etaplus.energy>
2025-04-28 14:24:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, jsonify
-
+from flask import Flask, request
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/')
-def home():
-    return 'Welcome to Centrica Flask App!'
-
-@app.route('/receive_centric_data', methods=['POST'])
-def receive_data():
+def index():
+    return "Server is up and running", 200
+ 
+@app.route('/data', methods=['POST'])
+def handle_data():
     data = request.json
-    print("Received Data:", data)
-    return jsonify({"status": "success", "message": "Data received successfully"}), 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    print(f"Data received: {data}")
+    return "Data received", 200
+ 
+if __name__ == "__main__":
+    import os
+    port = int(os.getenv("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
